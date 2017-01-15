08d658b4e34dc8a8e0d640cb67be4cbe95d458b6
Garisian Kana <garisian22@gmail.com>
2017-01-15 13:40:40 -0500

    added descriptions to images so description dynamically changes aslong with previous next

--- js/picRotatorWithButtons.js
+++ js/picRotatorWithButtons.js
@@ -12,15 +12,18 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var xX = document.getElementsByClassName("myDescriptions");
   var dots = document.getElementsByClassName("buttonSlideSShowButtons");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
+     xX[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" redSelectedButton", "");
   }
   x[slideIndex-1].style.display = "block";  
+  xX[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " redSelectedButton";
 }
